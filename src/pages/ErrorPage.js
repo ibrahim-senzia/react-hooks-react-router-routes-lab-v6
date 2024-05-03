6138947ca01ad50f259b87d7e1ad975d659115d0
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-05-03 16:36:02 +0300

    updated

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,16 @@
+import NavBar from "../components/NavBar"
+
+function ErrorPage() {
+  return (
+    <>
+        <header>
+            <NavBar />
+        </header>
+        <main>
+            <h1>Oops! Something went wrong.</h1>
+        </main>
+    </>
+  )
+}
+
+export default ErrorPage
